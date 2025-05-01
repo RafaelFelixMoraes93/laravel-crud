563ded7a703bf662f9e8ba981368e1c9a2a2a7ff
Rafael <rafael.felix.moraes@gmail.com>
2025-05-01 20:47:53 -0300

    Refactor adicionado rota delete

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/posts/read', [PostController::class,'read'] );
 Route::get('/posts/read_all', [PostController::class,'all'] );
 
 Route::get('/posts/update', [PostController::class,'update'] );
+
+Route::get('/posts/delete', [PostController::class,'delete'] );
