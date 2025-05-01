7d9f9d14d755cce74ff8b4dd09eecf7872ca4188
Rafael <rafael.felix.moraes@gmail.com>
2025-05-01 20:24:55 -0300

    Refactor adicionado rota update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/posts/create', [PostController::class, 'create'] );
 Route::get('/posts/read', [PostController::class,'read'] );
 
 Route::get('/posts/read_all', [PostController::class,'all'] );
+
+Route::get('/posts/update', [PostController::class,'update'] );
