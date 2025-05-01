4ad3db4871421ff8dbd69db10c46cb769a55391f
Rafael <rafael.felix.moraes@gmail.com>
2025-04-30 21:28:39 -0300

    Refactor criado função create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request) {
+        $new_post = [
+            'title' => 'Meu primeiro Post2',
+            'content'=> 'Conteúdo qualquer2',
+            'author' => 'Pedro2',
+        ];
+        $post = new Post($new_post);
+        $post->save();
+        dd($post);
+    }
 }
