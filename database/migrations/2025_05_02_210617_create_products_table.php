2add7ce4feecdea8391bd4bb58b23fe471bdc35a
Rafael <rafael.felix.moraes@gmail.com>
2025-05-02 18:15:46 -0300

    Refactor fazendo rollback

--- database/migrations/2025_05_02_210617_create_products_table.php
+++ database/migrations/2025_05_02_210617_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('nome');
             $table->string('codigo')->unique();
             $table->integer('quantidade')->default(0);
+            $table->text('obs')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
